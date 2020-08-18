00d3aba276bac24bd35cf4b2a1cda21179f7d8e4
Ben White <bwhite2140@outlook.com>
2020-08-18 16:56:57 -0700

    seed database

---
+++ AroundTheWorld/Migrations/20200818221548_SeedDatabase.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AroundTheWorld.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Destinations",
+                columns: new[] { "DestinationId", "City", "Country" },
+                values: new object[,]
+                {
+                    { 1, "Gothenburg", "Sweden" },
+                    { 2, "New York", "United States" },
+                    { 3, "Pompeii", "Italy" },
+                    { 4, "York", "England" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 1,
+                column: "DestinationId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 2,
+                column: "DestinationId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 3,
+                column: "DestinationId",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 4,
+                column: "DestinationId",
+                value: 4);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Destinations",
+                keyColumn: "DestinationId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Destinations",
+                keyColumn: "DestinationId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Destinations",
+                keyColumn: "DestinationId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Destinations",
+                keyColumn: "DestinationId",
+                keyValue: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 1,
+                column: "DestinationId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 2,
+                column: "DestinationId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 3,
+                column: "DestinationId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 4,
+                column: "DestinationId",
+                value: null);
+        }
+    }
+}
--- AroundTheWorld/Models/AroundTheWorldContext.cs
+++ AroundTheWorld/Models/AroundTheWorldContext.cs
@@ -9,12 +9,15 @@ namespace AroundTheWorld.Models
     public DbSet<Destination> Destinations { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
-      builder.Entity<Review>()
+      builder.Entity<Destination>()
         .HasData(
           new Destination { DestinationId = 1, Country = "Sweden", City = "Gothenburg" },
           new Destination { DestinationId = 2, Country = "United States", City = "New York" },
           new Destination { DestinationId = 3, Country = "Italy", City = "Pompeii" },
-          new Destination { DestinationId = 4, Country = "England", City = "York" },
+          new Destination { DestinationId = 4, Country = "England", City = "York" }
+        );
+      builder.Entity<Review>()
+        .HasData(
           new Review { ReviewId = 1, DestinationId = 1, Description = "The furniture was amazing", Rating = 9 },
           new Review { ReviewId = 2, DestinationId = 2, Description = "Nothing like the original", Rating = 3 },
           new Review { ReviewId = 3, DestinationId = 3, Description = "As someone who used to live there, its seen better days", Rating = 2 },
